571bc2ddcf07a1544f1ee9101f7474233e0689a1
Kay Ferraz <kayannesouza14@gmail.com>
2024-06-12 18:50:30 +0000

    db

---
+++ api/controllers/user.js
@@ -0,0 +1,11 @@
+import { dataBase } from "../dataBase.js";
+
+export const getUsers = (_, res) => {
+    const q = "SELECT * FROM usuarios";
+
+    dataBase.query(q, (err, data)=> {
+        if (err) return res.json(err);
+
+        return res.status(200).json(data);
+    });
+}
